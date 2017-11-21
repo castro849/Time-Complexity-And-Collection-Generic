934b2181078f673a89ac565a51b2d49a902890c6
maurocastro <maurobarbosacastro@gmail.com>
2017-11-21 22:10:57 +0000

    Publish

---
+++ NotationBig-O/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotationBig_O
+{
+    public class Person
+    {
+        public String FirstName { set; get; }
+        public String LastName { set; get; }
+        public String Company { set; get; }
+
+        public Person(String _firstname, String _lastname,String _company)
+        {
+            this.FirstName = _firstname;
+            this.LastName = _lastname;
+            this.Company = _company;
+        }
+        
+    }
+}
---
+++ NotationBig-O/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotationBig_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            list.Add(new Person("Mark", "Zuckerberg","Facebook"));
+            list.Add(new Person("Bill", "Gates", "Microsoft"));
+            list.Add(new Person("Steve", "Jobs", "Apple"));
+            var order_list = list.OrderBy(o => o.FirstName);
+            foreach (Person person in order_list)
+            {
+                Console.Out.WriteLine(String.Format("{0} {1}\t\t{2}", person.FirstName, person.LastName, person.Company));
+            }
+            Console.In.ReadLine();
+        }
+    }
+}
